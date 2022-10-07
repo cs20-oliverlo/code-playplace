f945824d10b8be92e07d837c44147907de5868a4
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-07 11:10:11 -0600

    awd

---
+++ main.js
@@ -0,0 +1,17 @@
+// Magic 8-Ball Start
+
+// Variables
+
+let ballQuestion = document.getElementById("8ball-question")
+let ballSpecificAnswer = document.getElementById("8ball-answer")
+
+// Event Listeners
+
+document.getElementById("ball-click").addEventListener("click", ballAnswer)
+
+// Function
+function ballAnswer() {
+    
+}
+
+// Magic 8-Ball End
