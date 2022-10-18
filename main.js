13fc4a286a66123133753e8fd33436718cd83ae3
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-10-18 10:32:48 -0600

    awd

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ function answer() {
 
 // Variables
 let fishImg = document.getElementById("fishy-img");
+let cloudImg = document.getElementById("cloth-img");
 
 // Set up canvas and 2D graphics context
 let cnv = document.getElementById("myCanvas");
@@ -173,14 +174,16 @@ function image1() {
 document.getElementById("draw-assignment1").addEventListener("click", assignment1);
 function assignment1() {
     ctx.clearRect(0, 0, cnv.width, cnv.height);
-    cnv.width = 400
-    cnv.height = 400
-    ctx.fillStyle = "rgb(10, 240, 0)";
-    ctx.fillRect(0, 300, 1000, 100);
-    ctx.fillStyle = "rgb(0, 0, 255)";
+    cnv.width = 400;
+    cnv.height = 400;
+    ctx.fillStyle = "blue";
     ctx.fillRect(0, 0, 1000, 300);
     ctx.fillStyle = "red";
     ctx.beginPath();
-    ctx.arc(200, 300, 25, 0, 1 * Math.PI);
+    ctx.arc(200, 300, 15, 0, 2 * Math.PI);
     ctx.fill();
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 300, 1000, 100);
+    ctx.drawImage(cloudImg, 150, 125);
+    ctx.drawImage(cloudImg, 175, 100);
 }
