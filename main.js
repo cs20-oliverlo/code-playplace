9548445f47cb6cbc850cb5a0b55251f2199caf82
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-12-06 11:04:34 -0700

    awd

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
 // Magic 8-Ball Start
 
 // Variables
-
 let ballSpecificAnswer = document.getElementById("8ball-answer");
 let ballAnswer = document.getElementById("8ball-answer");
 
 // Event Listeners
-
 document.getElementById("ball-click").addEventListener("click", answer);
 
 // Preset Answer Array
-
 const possible8BallAnswer = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good", "Yes.", "Signs point to yes." ,"Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."];
 
 
@@ -18,12 +15,12 @@ const possible8BallAnswer = ["It is certain.", "It is decidedly so.", "Without a
 function answer() {
     let ballQuestion = document.getElementById("8ball-question").value.toLowerCase();
 
-    if (ballQuestion === "do you say ay-arnav or arnav?") {
-        ballAnswer.innerHTML = "Ay-arnav";
+    if (ballQuestion === "how do you say quebec?") {
+        ballAnswer.innerHTML = "Ke-bek, not Kwe-bek";
     } else if (ballQuestion === "what is gagaball?") {
-        ballAnswer.innerHTML = "What.";
+        ballAnswer.innerHTML = "I don't know. I did not go to the rotunda to see the game.";
     } else if (ballQuestion === "how many if/else if statements did you do?") {
-        ballAnswer.innerHTML = "3, the else statement not included.";
+        ballAnswer.innerHTML = "3, the else statement at the end not included.";
     } else {
     let random = Math.trunc(Math.random() * possible8BallAnswer.length);
     ballAnswer.innerHTML = possible8BallAnswer[random];
